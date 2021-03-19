87f61672ecf2f3c0f4f5281a90c9239dd2ec3a28
Bruno Marcos Luz <brunoluz01.bl@gmail.com>
2021-03-18 21:47:53 -0300

    commit

--- dart fundamentos/variaveis.dart
+++ dart fundamentos/variaveis.dart
@@ -1,9 +1,16 @@
 // como declarar variáveis em Dart
 
+/* Dart é fortemente tipado, assim como Java, é uma linguagem robusta 
+e com muito divertida de aprender e programar
+
+podemos declarar o tipo da variável ou deixar que o dart infira
+o tipo a partir do dado em que passamos para ela. */
+
 main() {
   int num1 = 2;
   int num2 = 2;
-  int num3 = 2;
+  var num3 =
+      2; // dar vai inferir sobre o tipo do dado aqui! (no caso será inteiro)
   print((num1 + num2) ^ num3);
   print((num1 * num2) * num3);
 }
